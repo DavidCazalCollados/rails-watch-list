87979e5030919bffb0d271df4890b837c84a43c8
David Cazal <da.cazal95@gmail.com>
2024-11-23 13:11:33 -0500

    Challenge completed

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
 
   root to: "pages#home"
   resources :lists, except: [:edit, :update] do
-    resources :bookmarks, only: %i[new create index show]
+    resources :bookmarks, only: %i[new create]
   end
   resources :bookmarks, only: :destroy
   # Defines the root path route ("/")
